5fd8e8888c60ce44828136336fbca59e73d327f8
Omar Nassar <omar.portero.nassar1@gmail.com>
2023-12-24 09:07:29 +0100

    quick clean up

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -7,18 +7,19 @@ use App\Http\Dashboard\Auth\Controllers\LogoutController;
 
 Route::post('/login', [LoginController::class, 'adminLogin']);
 
-/*
-* Dashboard Get Routes
-*/
-
-Route::get('/statistics', [DashboardController::class, 'statistics']);
-Route::get('/clubs', [DashboardController::class, 'clubs']);
-Route::post('/clubs/{clubId}/upload-logo', [DashboardController::class, 'uploadLogo']);
-Route::post('/admin', [DashboardController::class, 'createAdmin']);
-
-
-
 Route::middleware('auth:admins')->group(function () {
-
+  //Logout
   Route::post('/logout', [LogoutController::class, 'adminLogout']);
+  
+  /*
+  * Dashboard Get Routes
+  */
+  Route::get('/statistics', [DashboardController::class, 'statistics']);
+  Route::get('/clubs', [DashboardController::class, 'clubs']);
+
+  //Add logo to club
+  Route::post('/clubs/{clubId}/upload-logo', [DashboardController::class, 'uploadLogo']);
+
+  //creation of admin
+  // Route::post('/admin', [DashboardController::class, 'createAdmin']);
 });
