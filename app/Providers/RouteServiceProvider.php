d48d180f8666a565b7593406c1028e78f426353e
Omar Nassar <omar.portero.nassar1@gmail.com>
2024-01-06 12:38:01 +0100

    Add route prefix for 'app'

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,9 +35,12 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
-                
+
             Route::prefix('dashboard')
                 ->group(base_path('routes/dashboard.php'));
+
+            Route::prefix('app')
+                ->group(base_path('routes/app.php'));
         });
     }
 }
