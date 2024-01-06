8e5c90c4c99e463f2f56dca9913667a211da76fc
Omar Nassar <omar.portero.nassar1@gmail.com>
2024-01-06 22:31:52 +0100

    migrations-fixed

--- app/Http/App/Controllers/AppRegistrationController.php
+++ app/Http/App/Controllers/AppRegistrationController.php
@@ -3,7 +3,9 @@
 namespace App\Http\App\Controllers;
 
 use App\Models\Club;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AppRegistrationController
 {
@@ -22,4 +24,33 @@ class AppRegistrationController
       'club' => $club,
     ];
   }
+
+  public function register(Request $request) {
+    $request->validate([
+      'voornamen' => 'required',
+      'achternaam' => 'required',
+      'email' => 'required|email,unique:users,email',
+    ]);
+
+    User::create([
+      'voornamen' => $request->voornamen,
+      'achternaam' => $request->achternaam,
+      'email' => $request->email,
+    ]);
+  }
+
+  public function registerFinish(Request $request) {
+    $request->validate([
+      'phone' => 'required',
+      'password' => 'required',
+      'password_confirmation' => 'required|same:password',
+    ]);
+
+    User::create([
+      'phone' => $request->phone,
+      'email' => $request->email,
+      'password' => Hash::make($request->password),
+    ]);
+  }
+    
 }
