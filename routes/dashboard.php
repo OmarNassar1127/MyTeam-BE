904bc393c070ed51856e0529fe044b6c52cd2d25
Omar Nassar <omar.portero.nassar1@gmail.com>
2024-01-07 09:56:50 +0100

    Fix issue with first_name and last_name in dashboard route

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -32,9 +32,6 @@ Route::middleware('auth:admins')->group(function () {
   Route::get('/get-managers', [DashboardController::class, 'getManagers']);
   Route::get('/get-clubs', [DashboardController::class, 'getClubs']);
 
-
-  // add te changes of first_name and last_name, it needs to be fixed!!
-
   /**
    * Clubs 
    */
