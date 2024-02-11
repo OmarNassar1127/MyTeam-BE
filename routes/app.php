cc472097276b9cf453911a234e48358cc0d58eae
Omar Nassar <omar.portero.nassar1@gmail.com>
2024-02-11 18:35:40 +0100

    Add AppLoginController and route for login functionality

--- routes/app.php
+++ routes/app.php
@@ -1,8 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\App\Controllers\AppLoginController;
 use App\Http\App\Controllers\AppRegistrationController;
 
 
+Route::post('/log-in', [AppLoginController::class, 'logIn']);
 
 Route::post('/fetch-club', [AppRegistrationController::class, 'clubFetch']);
 Route::post('/registration', [AppRegistrationController::class, 'register']);
